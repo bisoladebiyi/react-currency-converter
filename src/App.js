aab528750c171c1f3934b686533fd031b17a7653
abisola <abisola.ca@gmail.com>
2021-12-04 16:38:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const selectCurrency = (e) => {
+    console.log(e.target.outerText)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center">
+      <div className="h-screen w-1/2 ">
+      <div className="h-2/5 bg-dark text-white relative">
+        <div className="absolute bottom-0 w-full px-10 h-3/4">
+        <div className="flex justify-between items-center h-1/2">
+          <button className="text-blue font-bold text-lg" onClick={(e) => selectCurrency(e)}>NGN</button>
+          <p className="text-5xl">0</p>
+
+        </div>
+        
+        <div className="flex justify-between items-center h-1/2">
+        <button className="text-blue font-bold text-lg">USD</button>
+        <p className="text-5xl">0</p>
+        </div>
+        </div>
+      
+        
+      </div>
+        <div className=" bg-darker h-3/5 text-white grid grid-cols-3">
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">9</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">8</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">7</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">6</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">5</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">4</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">3</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">2</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">1</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">C</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">0</button>
+          <button className="w-full h-full hover:bg-white hover:bg-opacity-5">AC</button>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      dark: '#00223A',
+      darker: '#001B2F',
+      white: '#fff',
+      blue: '#98BAE7'
+
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
